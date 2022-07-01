98bb0dc9ad2c3f0a126d5beca69d66f143540a29
Liang <njl1@foxmail.com>
2022-07-02 00:28:21 +0800

    增加小程序版本更新提示

--- juan-card/miniprogram/app.js
+++ juan-card/miniprogram/app.js
@@ -2,7 +2,7 @@
 App({
   onLaunch: function () {
     if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+      console.error("请使用 2.2.3 或以上的基础库以使用云能力");
     } else {
       wx.cloud.init({
         // env 参数说明：
@@ -13,7 +13,23 @@ App({
         traceUser: true,
       });
     }
-    
+
+    // 立即更新版本,详见文档: https:/developers.weixin.qg.com/miniprogram/dev/framework/runtime,/update-mechanism.html
+    let updateManager = wx.getUpdateManager();
+    updateManager.onCheckForUpdate(function (res) {});
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: "更新提示",
+        content: "新版本已经准备好,是否重启应用？",
+        // showCancel: true,
+        success: (result) => {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
+
     this.globalData = {};
-  }
+  }, 
 });
