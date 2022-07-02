062357344075c885c88d28d098757efcd88e894f
Liang <njl1@foxmail.com>
2022-07-02 11:50:07 +0800

    增加：返回用户信息时为没有头像的用户设置临时默认头像

--- juan-card/cloudfunctions/fun/member/get.js
+++ juan-card/cloudfunctions/fun/member/get.js
@@ -1,14 +1,23 @@
 exports.main = async (event, context, cloud, db) => {
   console.log("member.get:", event);
 
+  const defaultAvatarUrl =
+    "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0";
+
   // 获取用户 OpenId
   const openid = cloud.getWXContext().OPENID;
   console.log(openid);
 
   // 查询
-  let member = await db.collection("member").doc(openid).get();
-  if (member.data) {
-    return member.data; // 已有用户信息，直接返回
+  let res = await db.collection("member").doc(openid).get();
+  let member = res.data;
+  if (member) {
+    // 校验是否有头像，没有头像的用户设置默认头像
+    if (isEmpty(member.avatar)) {
+      member.avatar = defaultAvatarUrl;
+    }
+    // 已有用户信息，终止执行并返回
+    return member;
   }
 
   // 新用户，初始化用户信息
@@ -23,9 +32,25 @@ exports.main = async (event, context, cloud, db) => {
     role: [],
   };
 
-  // 创建
+  // 创建用户信息
   await db.collection("member").add({
     data: form,
   });
+
+  // 设置默认头像并返回
+  form.avatar = defaultAvatarUrl;
   return form;
 };
+
+function isEmpty(str) {
+  // 正则去除字符串空格
+  let res = str.replace(/\s+/g, "");
+  return (
+    res === "" ||
+    !res.length ||
+    !res ||
+    res === undefined ||
+    res === "null" ||
+    res === "undefined"
+  );
+}
