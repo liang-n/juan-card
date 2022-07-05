cac42659108f0f9159671148b6e3cccf7e8a84bd
Liang <njl1@foxmail.com>
2022-07-06 01:05:01 +0800

    - 首页 UI 绑定会员信息和活动详情的动态数据 - 首页数据加载逻辑调整为先从缓存中加载减少用户白屏等待时间，然后再后台获取实时数据覆盖

--- juan-card/miniprogram/pages/index/index.js
+++ juan-card/miniprogram/pages/index/index.js
@@ -1,10 +1,10 @@
 Page({
   data: {
-    tips: "记得打卡哦~",
-    checkName: "立即打卡",
+    // tips: "记得打卡哦~",
+    // checkName: "立即打卡",
     member: null,
     record: null,
-    isAmin: false,
+    isAdmin: false,
   },
 
   onShow: function (e) {
@@ -12,33 +12,81 @@ Page({
   },
 
   init: async function (e) {
+    // this.clearStorage();
+
+    wx.showLoading();
+
+    // 读取缓存中的数据，减少用户等待时间
+    // let storageList = wx.batchGetStorageSync(["member", "record"]);
+    // if (storageList) {
+    this.setData({
+      member: wx.getStorageSync("member"),
+      record: wx.getStorageSync("record"),
+    });
+    // }
+
+    wx.hideLoading();
+
+    // 读取实时数据
+    await this.getRecord();
     this.setData({
       member: await getApp().getMember(),
     });
-    // await this.getRecord();
-    // this.isAdmin(this.data.member.openid, this.data.record);
+    this.isAdmin(this.data.member, this.data.record);
   },
 
   getRecord: async function (e) {
     try {
+      // 查询活动详情
       let res = await wx.cloud.callFunction({
         name: "fun",
         data: {
           type: "getRecord",
         },
       });
+      let record = res.result.data[0];
+
+      // 格式化日期: 2022年07月06日 - 08月06日
+      const startDate = new Date(record.startDate);
+      const endDate = new Date(record.endDate);
+      let _strDateRange = `${startDate.getFullYear()}年${
+        startDate.getMonth() + 1
+      }月${startDate.getDate()}日 - ${
+        endDate.getMonth() + 1
+      }月${endDate.getDate()}`;
+      record._strDateRange = _strDateRange;
+
+      // 计算活动状态
+      const now = Date.now();
+      const start = startDate.getTime();
+      const end = endDate.getTime();
+      console.log("now", now, "start", start, "end", end);
+      let _status;
+      if (now < start) {
+        _status = "即将开始";
+      } else if (now > end) {
+        _status = "已结束";
+      } else {
+        _status = "进行中";
+      }
+      record._status = _status;
+
+      // 写入 Data 和本地缓存
       this.setData({
-        record: res.result.data[0],
+        record,
       });
+      wx.setStorageSync("record", record);
     } catch (error) {
-      console.log("getRecord.error".error);
+      console.error("getRecord.error".error);
     } finally {
       console.log("record", this.data.record);
     }
   },
 
-  isAdmin: function (openid, record) {
-    let isAdmin = record.leader?.indexOf(openid) > -1;
+  isAdmin: function (member, record) {
+    let isAdmin =
+      record.leader?.indexOf(member.openid) > -1 ||
+      this.member.role.indexOf("admin") !== -1;
     console.log("isAdmin", isAdmin);
     this.setData({
       isAdmin,
@@ -50,25 +98,5 @@ Page({
     wx.showToast({
       title: "清除缓存成功",
     });
-  },
-
-  login: function () {
-    // wx.login().then((res) => {
-    //   console.log(res);
-    //   wx.cloud.callFunction({
-    //     name: "fun",
-    //     data: {
-    //       type: "login",
-    //       data: {
-    //         code: res.code,
-    //       },
-    //     },
-    //   }).then(console.log);
-    //   // this.init();
-    // });
-    wx.getSetting().then(console.log)
-    wx.authorize({
-      scope: "scope.camera",
-    }).then(console.log);
   },
 });
